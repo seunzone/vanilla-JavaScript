b27c162728ce8c25ed8b7e7dbd4dce326d3c5d41
seunzone <darealseun@gmail.com>
2017-10-29 15:14:50 +0100

    function to calculate factorial in JavaScript

--- factorial.js
+++ factorial.js
@@ -65,4 +65,25 @@ function factorial(num) {
 }
    
 factorial(4);  
+//========================================================//
 
+//========================================================//
+
+//========================================================//
+
+// 3rd Method
+
+function FirstFactorial(num) { 
+  
+  if (num === 0 || num === 1) {
+    return 1;
+  }
+  else {
+   	return num * FirstFactorial(num - 1); 
+  }      
+}
+   
+// keep this function call here 
+// to see how to enter arguments in JavaScript scroll down
+FirstFactorial(10);
+FirstFactorial(9);
